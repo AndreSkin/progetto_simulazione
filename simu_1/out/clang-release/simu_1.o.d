f06bba542547def8c3111dac9d339fdaf5844020
Andrea Schinoppi <andrea.schinoppi@studio.unibo.it>
2022-10-27 10:17:15 +0200

    First commit

---
+++ simu_1/out/clang-release/simu_1.o.d
@@ -0,0 +1,4 @@
+out/clang-release//simu_1.o: simu_1.cc \
+  C:/Users/schin/Downloads/omnetpp-6.0.1-windows-x86_64/omnetpp-6.0.1/include/omnetpp.h
+
+C:/Users/schin/Downloads/omnetpp-6.0.1-windows-x86_64/omnetpp-6.0.1/include/omnetpp.h:
---
+++ simu_1/simu_1.cc
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
